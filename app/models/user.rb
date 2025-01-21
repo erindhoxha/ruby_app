e9fd6217e8d79db244aa287f1d079e84a3888318
erindhoxha <hello@erindhoxha.dev>
2025-01-21 14:11:12 +0100

    Db

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
   has_many :articles
+  validates :username, presence: true, length: { minimum: 3, maximum: 25 }, uniqueness: { case_sensitive: false }
+  validates :email, presence: true, length: { maximum: 105 }, uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 6, maximum: 25 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -24,6 +24,7 @@ ActiveRecord::Schema[7.0].define(version: 2025_01_21_125812) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email"
+    t.string "password"
   end
 
 end
