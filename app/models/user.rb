a553f38e826519f749b283f7aeab47bf6d82103a
erindhoxha <hello@erindhoxha.dev>
2025-01-21 19:12:34 +0100

    Users

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,5 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { maximum: 105 },
             uniqueness: { case_sensitive: false },
             format: { with: VALID_EMAIL_REGEX }
-  validates :password, presence: true, length: { minimum: 6, maximum: 25 }
   has_secure_password
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
   get 'login', to: 'pages#login'
-  get 'signup', to: 'pages#signup'
+  get 'signup', to: 'users#new'
 end
