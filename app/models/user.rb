44dfb143f58dd0bb7a68c3d92d96712ab820f127
erindhoxha <hello@erindhoxha.dev>
2025-01-21 16:13:23 +0100

    Validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   has_many :articles
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :username, presence: true, length: { minimum: 3, maximum: 25 }, uniqueness: { case_sensitive: false }
-  validates :email, presence: true, length: { maximum: 105 }, uniqueness: { case_sensitive: false }
+  validates :email, presence: true, length: { maximum: 105 },
+            uniqueness: { case_sensitive: false },
+            format: { with: VALID_EMAIL_REGEX }
   validates :password, presence: true, length: { minimum: 6, maximum: 25 }
 end
