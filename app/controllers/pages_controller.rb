2dd36cc0fb65f1d44265bec979de886fad81dc72
erindhoxha <hello@erindhoxha.dev>
2025-02-01 20:40:52 +0100

    Added search

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,4 +5,14 @@ class PagesController < ApplicationController
 
   def about
   end
+
+  def search
+    if params[:search].present?
+      @users = User.where("username LIKE ?", "%#{params[:search]}%")
+      @articles = Article.where("title LIKE ? OR description LIKE ?", "%#{params[:search]}%", "%#{params[:search]}%")
+    else
+      @users = []
+      @articles = []
+    end
+  end
 end
