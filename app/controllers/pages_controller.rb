4430985e8c508cc4ca924f5acb2ee96feb8832be
erindhoxha <hello@erindhoxha.dev>
2025-02-01 20:45:13 +0100

    Test

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,8 +8,9 @@ class PagesController < ApplicationController
 
   def search
     if params[:search].present?
-      @users = User.where("username ILIKE ?", "%#{params[:search]}%")
-      @articles = Article.where("title ILIKE ? OR description ILIKE ?", "%#{params[:search]}%", "%#{params[:search]}%")
+      search_term = "%#{params[:search].downcase}%"
+      @users = User.where("LOWER(username) LIKE ?", search_term)
+      @articles = Article.where("LOWER(title) LIKE ? OR LOWER(description) LIKE ?", search_term, search_term)
     else
       @users = []
       @articles = []
