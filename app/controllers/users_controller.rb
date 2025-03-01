b668cd2f91115e0dcfaa3483a5a853859a050d29
erindhoxha <hello@erindhoxha.dev>
2025-03-01 14:39:51 +0100

    Added delete flash notice

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -65,7 +65,7 @@ class UsersController < ApplicationController
     @user.destroy
     session.delete(:user_id)
     respond_to do |format|
-      format.html { redirect_to users_path, status: :see_other, notice: "User was successfully destroyed." }
+      format.html { redirect_to users_path, status: :see_other, notice: "User was successfully deleted." }
       format.json { head :no_content }
     end
   end
