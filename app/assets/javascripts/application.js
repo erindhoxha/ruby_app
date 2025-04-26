72e8c20dd69e09cf0eeddd76ae8a0816ca361278
erindhoxha <hello@erindhoxha.dev>
2025-04-26 15:20:35 +0200

    Added turbolinks:load

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,27 +1,29 @@
-// filepath: /Users/erindhoxha/Documents/prjcts/store/app/assets/javascripts/application.js
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("turbolinks:load", () => {
   const confirmationModal = document.getElementById("exampleModal");
   const confirmDeleteButton = document.getElementById("confirmDelete");
 
-  confirmationModal.addEventListener("show.bs.modal", (event) => {
-    const button = event.relatedTarget;
-    const articleId = button.getAttribute("data-article-id");
-    confirmDeleteButton.setAttribute("data-article-id", articleId);
-  });
+  if (confirmationModal && confirmDeleteButton) {
+    confirmationModal.addEventListener("show.bs.modal", (event) => {
+      const button = event.relatedTarget;
+      console.log("LOAD");
+      const articleId = button.getAttribute("data-article-id");
+      confirmDeleteButton.setAttribute("data-article-id", articleId);
+    });
 
-  confirmDeleteButton.addEventListener("click", () => {
-    const articleId = confirmDeleteButton.getAttribute("data-article-id");
-    const form = document.createElement("form");
-    form.method = "POST";
-    form.action = `/articles/${articleId}`;
-    form.innerHTML = `<input name="_method" value="delete" type="hidden"><input name="authenticity_token" value="${
-      document.querySelector('meta[name="csrf-token"]').content
-    }" type="hidden">`;
-    document.body.appendChild(form);
-    form.submit();
-  });
+    confirmDeleteButton.addEventListener("click", () => {
+      const articleId = confirmDeleteButton.getAttribute("data-article-id");
+      const form = document.createElement("form");
+      form.method = "POST";
+      form.action = `/articles/${articleId}`;
+      form.innerHTML = `<input name="_method" value="delete" type="hidden"><input name="authenticity_token" value="${
+        document.querySelector('meta[name="csrf-token"]').content
+      }" type="hidden">`;
+      document.body.appendChild(form);
+      form.submit();
+    });
+  }
 });
