c67446c272279b851dba6bf880360fb1f402315a
erindhoxha <hello@erindhoxha.dev>
2025-12-29 23:09:47 +0100

    Add controller tests

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,5 @@ Rails.application.routes.draw do
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
   get "search", to: "pages#search"
+  resources :categories, except: [:destroy]
 end
