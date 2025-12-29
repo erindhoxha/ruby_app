2dffb9a12d84131b8d727c0b24d131670bd019cb
erindhoxha <hello@erindhoxha.dev>
2025-12-29 22:46:42 +0100

    Learn setup

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -2,14 +2,17 @@ require "test_helper"
 
 class CategoryTest < ActiveSupport::TestCase
  # "Category should be valid"
+ def setup
+  @category = Category.new(name: "Electronics")
+ end
+
  test "category should be valid" do
-  @category = Category.new(name: "Sports")
   assert @category.valid?
  end
 
  test "category name should be present" do
-  @category = Category.new(name: "");
-  assert @category.valid?
+  @category.name = " "
+  assert_not @category.valid?
  end
  
 end
